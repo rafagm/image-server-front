ded59e1f41462aad938385ae26f6130208ce49de
rafael.guardeno <rafael.guardeno.marquez@gmail.com>
2020-09-16 09:08:21 +0200

    Infinite scroll working on scrollUp and scrollDown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,9 @@ export class AppComponent {
   // It's used to prevent memory leaks because of the infinite scroll.
   private imagesSize;
 
+  private scrolledUp = false;
+  private scrolledDownCounter = 0;
+
   showSpinner = true;
 
   constructor(private imageService: ImageService) {
@@ -29,7 +32,7 @@ export class AppComponent {
     const width = (window.innerWidth > 0) ? window.innerWidth : screen.width; //get the screen's width
 
     if (width <= 475)
-      this.pageSize = 10;
+      this.pageSize = 5;
     else if (width <= 1150)
       this.pageSize = 5;
     else
@@ -44,28 +47,49 @@ export class AppComponent {
       .subscribe((images: Image[]) => (this.images = images));
   }
 
-  getALLImagesPagination(pageNo?, pageSize?, sortBy?) {
+  getALLImagesPagination(pageNo?, pageSize?, sortBy?, scrolledUp = false) {
+    this.showSpinner = true;
+
+    if (this.scrolledUp && !scrolledUp) {
+      pageNo = this.pageNo + Math.floor(this.imagesSize / this.pageSize);
+      this.scrolledUp = false;
+    } else if (this.scrolledDownCounter >= 2 && scrolledUp) {
+      pageNo = this.pageNo - Math.floor(this.imagesSize / this.pageSize);
+      this.scrolledDownCounter = 0;
+    }
+
+    pageNo = Math.max(0, pageNo);
+
     setTimeout(() => {
       this.imageService
       .getAllImagesPagination(pageNo, pageSize, sortBy)
       .subscribe((images: Image[]) => {
         this.showSpinner = false;
-        this.pageNo++;
 
-        this.images = this.images.concat(images);
-        this.images = this.images.slice(Math.max(0, (this.images.length - this.imagesSize)));
+        this.pageNo = pageNo;
+
+        if (!scrolledUp)  {
+          this.images = this.images.concat(images);
+          this.images = this.images.slice(Math.max(0, (this.images.length - this.imagesSize)));
+        } else {
+          this.images = images.concat(this.images);
+          this.images = this.images.slice(0, Math.min(this.images.length, this.imagesSize));
+        }
 
       });
     }, 200);
   }
 
   onScroll() {
-    this.showSpinner = true;
-    this.getALLImagesPagination(this.pageNo, this.pageSize);
+    this.scrolledDownCounter++;
+    this.getALLImagesPagination(this.pageNo + 1, this.pageSize);
   }
 
   onScrollUp() {
-    console.log("Scrolled Up");
+    if (this.pageNo !== 0) {
+      this.scrolledUp = true;
+      this.getALLImagesPagination(this.pageNo - 1, this.pageSize, null, true);
+    }
 
   }
 
